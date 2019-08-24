2e41df906d69f72b3b19f293587137262601dd85
HanxunHuangLemonBear <curtishuang@foxmail.com>
2019-08-24 13:55:28 +1000

    [UPDATE] Feature Extraction

---
+++ script/feature_extract_v1_1132.sh
@@ -0,0 +1,6 @@
+cd ../
+python3 coconut_train.py --features_extract  \
+                            --train_set_file_path data/feature_extract_data_1132/train_set_v1_1_more_than_55.txt  \
+                            --dev_set_file_path data/feature_extract_data_1132/dev_set_v1_1_more_than_55.txt  \
+                            --idx_file_path data/feature_extract_data_1132/v1_idx_more_than_55.pickle \
+                            --num_of_classes  1132
