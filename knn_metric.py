95aee19d3c236bf16ac7a6a422f3c8c496cbf053
Weizhuo Zhang <wzhuo.zhang@gmail.com>
2019-08-28 15:01:25 +1000

    finish knn metric

--- knn_metric.py
+++ knn_metric.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
-train_set_path = "/Users/weizhuozhang/Desktop/Files/train_encode_tuned.npy"
-dev_set_path = "/Users/weizhuozhang/Desktop/Files/dev_encode_tuned.npy"
-train_set_csv = "/Users/weizhuozhang/Desktop/Files/train_set_v1.txt"
-dev_set_csv = "/Users/weizhuozhang/Desktop/Files/dev_set_v1.txt"
+train_set_path = "/home/weizhuozhang/Desktop/cwb/COMP90051-Project1/data/BERT_encoding/train_encode_tuned.npy"
+dev_set_path = "/home/weizhuozhang/Desktop/cwb/COMP90051-Project1/data/BERT_encoding/dev_encode_tuned.npy"
+train_set_csv = "/home/weizhuozhang/Desktop/cwb/COMP90051-Project1/data/train_set_v1.txt"
+dev_set_csv = "/home/weizhuozhang/Desktop/cwb/COMP90051-Project1/data/dev_set_v1.txt"
 
 train_set = np.load(train_set_path)
 train_csv = pd.read_csv(train_set_csv, sep='\t', header=None)
@@ -18,15 +18,16 @@ train_label = (train_csv[:,0]).astype('int')
 dev_label = (dev_csv[:,0]).astype('int')
 
 import time
-k_neighbors = [3, 7, 9, 13, 15]
+k_neighbors = [25, 30, 35, 40, 45, 50]
 for k in k_neighbors:
+    print("---------------------------------")
+    print("start knn k={0}".format(k))
     knn_clf = KNeighborsClassifier(n_jobs=-1, n_neighbors=k)
     knn_clf.fit(train_set, train_label)
     time_start=time.time()
     predicted = knn_clf.predict(dev_set)
     accuracy = sum(predicted == dev_label)/len(predicted)
     time_end=time.time()
-    print("---------------------------------")
     print("knn k={0}".format(k))
     print("accuracy: {0}".format(accuracy))
     print("Time spent: {0:.2f}ms".format((time_end-time_start)*1000))
